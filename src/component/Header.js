6640acf816f86521d5d5c8af35ca2d27164cd238
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-03-19 07:19:38 +0530

    signIN with firebase

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Icon from "@mdi/react";
 import { mdiMenu, mdiYoutube, mdiMagnify, mdiMicrophone, mdiBellOutline } from "@mdi/js";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
 import {app} from "../Firebase.js"
 
 function Header() {
   const [user, setUser] =useState();
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        setUser(user);
+      } 
+    });
+  },[])
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
@@ -14,7 +23,7 @@ function Header() {
     .then((result) => {
       
       const user = result.user;
-      setUser(user);
+      
       console.log(user)
       // IdP data available using getAdditionalUserInfo(result)
       // ...
@@ -24,27 +33,28 @@ function Header() {
     });
   }
 
-
+  
 
 
 
   return (
     <div className="containor">
+      {/* <img src="download.png" alt="" /> */}
       <div className="header-left"> <Icon path={mdiMenu} size={1.5} style={{cursor: "pointer"}} />
-      <Icon className="icon" path={mdiYoutube} size={1.5} color={"red"} /> <span style={{cursor: "pointer"}}> Youtube <sup>IN</sup></span>
+      <Icon className="icon" path={mdiYoutube} size={1.5} color={"red"} /> <span style={{cursor: "pointer"}}> Youtube <sup style={{fontSize:"10px", display:"inlineBlock"}}>IN</sup></span>
       </div>
 
       <div className="header-center">
-      <input className="search" type="search" placeholder="Search" />
-      <Icon className="s-icon" path={mdiMagnify} size={1.5} />
+      <input className="search" type="search" placeholder="Search"  />
+      <Icon className="s-icon" path={mdiMagnify} size={1.5}  />
       <Icon  path={mdiMicrophone} size={1.5} style={{cursor: "pointer"}}/>
     </div>
     <div className="header-right">
-        <Icon path={mdiBellOutline} size={1.5} style={{cursor: "pointer"}}/>
+        <Icon path={mdiBellOutline} size={1.4} style={{cursor: "pointer",  marginRight:"15px"}}/>
         {
-        user ? <img style={{height:"30px", width:"30px", borderRadius:"50%"}} src={user.photoURL} alt="image" />: 
+        user ? <img style={{height:"35px", width:"35px", borderRadius:"50%"}} src={user.photoURL} alt="image" />: 
         <button style={{cursor: "pointer"}} onClick={handleClick}>Sign in</button>
-}
+        }
     </div>
     </div>
   );
